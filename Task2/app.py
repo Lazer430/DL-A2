988db3717e7861bad3a676b2c61ee35c7f55e2ed
Lazer430 <fasihabdullah430@gmail.com>
2023-10-21 11:32:35 +0500

    improved performance

--- Task2/app.py
+++ Task2/app.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from flask import Flask, render_template, request, jsonify
 import tensorflow as tf
 import numpy as np
@@ -6,10 +7,13 @@ import signal
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
+import yfinance as yf
 
 device = None
 model = tf.keras.models.load_model("task2.keras")
 scaler = MinMaxScaler(feature_range=(0, 1))
+dateToPredict = None
+dateToPredictNext = None
 
 cols = ["Open", "High", "Low", "Close", "Adj Close", "Volume"]
 numberOfInputDays = 7
@@ -26,7 +30,12 @@ def predict(model, data):
 
 
 def preprocessing(data):
+    global dateToPredict, dateToPredictNext
     data = data.ffill()
+    dateToPredict = data["Date"].tail(1).values[0]
+    dateToPredict = pd.to_datetime(dateToPredict)
+    # dateToPredict = dateToPredict + pd.DateOffset(days=1)
+    dateToPredictNext = dateToPredict + pd.DateOffset(days=2)
     data = data.drop(["Date"], axis=1)
     if data.isna().sum().sum() != 0:
         data = data.bfill()
@@ -58,12 +67,21 @@ def upload():
         last_7_days = preprocessing(last_7_days)
         last_7_days = np.array(last_7_days)
         prediction = predict(model, last_7_days)
-        # convert prediction to string
-        prediction = str(prediction)
+
+        print("Date to predict: ", dateToPredict)
+        print("Date to predict next: ", dateToPredictNext)
+
+        actualData = yf.download("AAPL", start=dateToPredict, end=dateToPredictNext)
+
+        # if "Date" in actualData.columns:
+        #     actualData = actualData.Drop(["Date"], axis=1)
+        actualData = str(actualData[1:])
+        prediction = str(prediction[0])
+
     else:
         return render_template("index.html", prediction="No file uploaded")
 
-    return render_template("index.html", prediction=prediction)
+    return render_template("index.html", prediction=prediction, actualData=actualData)
 
 
 # Run the Flask app
